01939dd3b1d41e991b1a7844a722c3970e6fc804
Alejandro Lopez <galopez683@misena.edu.co>
2016-08-26 20:15:41 -0500

    desarrollo punto 2

---
+++ src/edu/alejozepol/Punto2/ColeccionEnteros.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.alejozepol.Punto2;
+
+import edu.alejozepol.ejerciciospoo.Menu;
+import static java.lang.Math.max;
+import java.util.Random;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alejozepol
+ */
+public class ColeccionEnteros {
+
+    private int numero, max, min;
+    private int[] conjuntoEnteros;
+    private Random rnd = new Random();
+    private String entrada;
+
+    public ColeccionEnteros(int numero, int[] conjuntoEnteros) {
+        this.numero = numero;
+        this.conjuntoEnteros = conjuntoEnteros;
+    }
+
+    public ColeccionEnteros() {
+
+    }
+
+    public void entrada() {
+        entrada = JOptionPane.showInputDialog(null, "Ingrese la cantidad de numeros que llenar el arreglo:");
+
+        if (entrada != null && !entrada.contentEquals("")) {
+            numero = (Integer.parseInt(entrada));
+            conjuntoEnteros = new int[numero];
+
+            for (int i = 0; i < conjuntoEnteros.length; i++) {
+                conjuntoEnteros[i] = rnd.nextInt(101);
+
+            }
+        } else {
+            Menu.Menu();
+        }
+
+    }
+
+    public void desplegarColeccion() {
+        for (int i = 0; i < conjuntoEnteros.length; i++) {
+            System.out.println(conjuntoEnteros[i]);
+        }
+
+    }
+
+    public void existeValor() {
+
+        for (int i = 0; i < conjuntoEnteros.length; i++) {
+            if (conjuntoEnteros[i] == numero) {
+                JOptionPane.showMessageDialog(null, "El numero " + conjuntoEnteros[i] + " hace parte de los numeros aleatorios");
+            }
+        }
+
+    }
+
+    public void maximoValor() {
+        numero = numero - 1;
+        max = min = conjuntoEnteros[numero];
+
+        for (int i = 0; i < conjuntoEnteros.length; i++) {
+            if (conjuntoEnteros[i] > max) {
+                max = conjuntoEnteros[i];
+                JOptionPane.showMessageDialog(null, "El numero " + conjuntoEnteros[i] + " es el numero maximo almacenado");
+            }
+        }
+
+    }
+
+    public void desplegarPosicionesMultiplo(int num) {
+        entrada = JOptionPane.showInputDialog(null, "Ingrese el numeros para verificar los indices que son multiples de él:");
+        if (entrada != null && !entrada.contentEquals("")) {
+            num = (Integer.parseInt(entrada));
+            for (int i = 0; i < conjuntoEnteros.length; i++) {
+                int resto = i % num;
+
+                if (resto == 0) {
+                    JOptionPane.showMessageDialog(null, " El numero " + conjuntoEnteros[i] + " almacenado en el indice " + i + " es multiple de "
+                            + num);
+                }
+            }
+        }
+
+    }
+
+    public void promedioValores(int total, int promedio) {
+        for (int i = 0; i < conjuntoEnteros.length; i++) {
+            total = total + conjuntoEnteros[i];
+        }
+        promedio = total / conjuntoEnteros.length;
+        JOptionPane.showMessageDialog(null, " El promedio de los numeros generados son: total numeros: " + total + " dividido entre " + conjuntoEnteros.length
+                + " para un total de: " + promedio);
+
+    }
+
+    public void invertir() {
+
+        Object[] invertir_int = new Object[conjuntoEnteros.length];
+        int maximo = conjuntoEnteros.length;
+
+        for (int i = 0; i < conjuntoEnteros.length; i++) {
+            invertir_int[maximo - 1] = conjuntoEnteros[i];
+            maximo--;
+        }
+
+        for (int i = 0; i < invertir_int.length; i++) {
+            Object j = invertir_int[i];
+            System.out.println("Arreglo invertido: " + j);
+
+        }
+
+    }
+
+    public void duplicarCeldasMultiplo(int num, int resto) {
+        entrada = JOptionPane.showInputDialog(null, "Ingrese un numero:");
+        if (entrada != null && !entrada.contentEquals("")) {
+            num = (Integer.parseInt(entrada));
+            for (int i = 0; i < 10; i++) {
+                resto=num%conjuntoEnteros[i];
+                if (resto==0) {
+
+                    JOptionPane.showMessageDialog(null, " El numero " + conjuntoEnteros[i] + " almacenado en el indice " + i + " es multiple de "
+                            + num);
+                }
+            }
+        }
+    }
+
+}
+
+
---
+++ src/edu/alejozepol/Punto2/Punto2.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.alejozepol.Punto2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alejozepol
+ */
+public class Punto2 {
+
+    public static void punto2() {
+
+        JOptionPane.showMessageDialog(null, "Punto 2: Se desea programar una clase que permita\n"
+                + "representar una colección acotada de valores enteros. Para almacenar los valores\n"
+                + " de la colección, la clase utilizará como único atributo un arreglo de acuerdo\n"
+                + "con la siguiente declaración:\n"
+                + " \n"
+                + "public class ColeccionEnteros {  private int [] arreglo;...} \n"
+                + "\nEl método constructor de la clase deberá recibir como parámetro el tamaño que\n"
+                + "deberá tener el arreglo. Dentro del método, se deberá crear el arreglo con esa\n"
+                + "cantidad de celdas y posteriormente se deberán cargar las mismas con valores generados\n"
+                + "al azar. Además del método constructor, la clase deberá proveer los siguientes métodos:  \n"
+                + "• DesplegarColeccion ( ). Este método deberá desplegar en pantalla\n"
+                + "todos los valores almacenados en el arreglo.  \n"
+                + "• ExisteValor (int valor). Este método deberá determinar si el\n "
+                + "valor recibido como parámetro forma parte del arreglo o no.  \n"
+                + "• MaximoValor ( ). Este método deberá devolver el máximo valor\n"
+                + "almacenado en el arreglo.  \n"
+                + "• DesplegarPosicionesMultiplo (int num). Este método deberá desplegar\n"
+                + "en pantalla todos los valores del arreglo almacenados en celdas cuyo\n"
+                + "índice sea múltiplo de num.  \n"
+                + "• PromedioValores ( ). Este método deberá calcular el promedio\n"
+                + "de todos los valores almacenados en el arreglo.  \n"
+                + "• Invertir ( ). Este método deberá invertir los valores almacenados\n"
+                + "en el arreglo. Es decir, el valor de la primera celda deberá intercambiarse\n"
+                + "con el valor de la última celda. El valor de la segunda celda deberá\n"
+                + "intercambiarse con el valor de la penúltima celda, y así sucesivamente.  \n"
+                + "• DuplicarCeldasMultiplo (int valor). Este método deberá duplicar el \n"
+                + "valor de cada celda del arreglo que contenga un elemento que sea múltiplo\n"
+                + "del valor recibido como parámetro.  \n"
+                + "• HayRepetidos ( ). Este método deberá determinar si el arreglo posee al\n"
+                + " menos un valor repetido en más de una celda o no. ");
+
+        ColeccionEnteros coleccion = new ColeccionEnteros();
+
+        coleccion.entrada();
+        JOptionPane.showMessageDialog(null, "Acontinuacion se imprimira por Consolda el listado de nuemeros generados:");
+        coleccion.desplegarColeccion();
+        coleccion.existeValor();
+        coleccion.maximoValor();
+        coleccion.desplegarPosicionesMultiplo(0);
+        coleccion.promedioValores(0, 0);
+        coleccion.invertir();
+    }
+
+}
